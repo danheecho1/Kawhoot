bce887f28bcbbcf8a482d300afb213de32add266
Danny Cho <danheecho@gmail.com>
2022-12-04 01:59:24 -0800

    usersummaries updated to include updated_at timestamp for each leaderboard criterion
    leaderboard was sorting by username when scores were the same. table updated to sort by updated_at timestamp when scores are the same.

--- kawhoot_app/models/userSummary_model.py
+++ kawhoot_app/models/userSummary_model.py
@@ -5,25 +5,28 @@ class UserSummary:
     def __init__(self, data): 
         self.id = data['id']
         self.total_score_earned = data['total_score_earned']
+        self.total_score_earned_updated_at = data['total_score_earned_updated_at']
         self.average_score = data['average_score']
+        self.average_score_updated_at = data['average_score_updated_at']
         self.attempt_count = data['attempt_count']
+        self.attempt_count_updated_at = data['attempt_count_updated_at']
         self.create_quiz_count = data['create_quiz_count']
+        self.create_quiz_count_updated_at = data['create_quiz_count_updated_at']
         self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
 
     @classmethod
     def create_user_summary(cls, data): 
-        query = "INSERT INTO usersummaries (total_score_earned, average_score, attempt_count, create_quiz_count, created_at, updated_at, user_id) VALUES (0, 0, 0, 0, NOW(), NOW(), %(user_id)s);"
+        query = "INSERT INTO usersummaries (total_score_earned, total_score_earned_updated_at, average_score, average_score_updated_at, attempt_count, attempt_count_updated_at, create_quiz_count, create_quiz_count_updated_at, created_at, user_id) VALUES (0, NOW(), 0, NOW(), 0, NOW(), 0, NOW(), NOW(), %(user_id)s);"
         return connectToMySQL('kawhoot_schema').query_db(query, data)
 
     @classmethod
     def update_user_summary(cls, data):
-        query = "UPDATE usersummaries SET total_score_earned = total_score_earned + %(score)s, attempt_count = attempt_count + 1, average_score = (total_score_earned / attempt_count), updated_at = NOW() WHERE user_id = %(user_id)s;"
+        query = "UPDATE usersummaries SET total_score_earned = total_score_earned + %(score)s, total_score_earned_updated_at = NOW(), attempt_count = attempt_count + 1, attempt_count_updated_at = NOW(), average_score = (total_score_earned / attempt_count), average_score_updated_at = NOW() WHERE user_id = %(user_id)s;"
         return connectToMySQL('kawhoot_schema').query_db(query, data)
     
     @classmethod
     def update_user_summary_create_quiz(cls, data):
-        query = "UPDATE usersummaries SET create_quiz_count = create_quiz_count + 1, updated_at = NOW() WHERE user_id = %(user_id)s;"
+        query = "UPDATE usersummaries SET create_quiz_count = create_quiz_count + 1, create_quiz_count_updated_at = NOW() WHERE user_id = %(user_id)s;"
         return connectToMySQL('kawhoot_schema').query_db(query, data)
 
     @classmethod
@@ -36,20 +39,20 @@ class UserSummary:
 
     @classmethod
     def get_scorehunters(cls):
-        query = "SELECT username, total_score_earned FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE total_score_earned > 0 ORDER BY total_score_earned DESC LIMIT 5;"
+        query = "SELECT username, total_score_earned FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE total_score_earned > 0 ORDER BY total_score_earned DESC, total_score_earned_updated_at ASC LIMIT 5;"
         return connectToMySQL('kawhoot_schema').query_db(query)
 
     @classmethod
     def get_steadyhunters(cls):
-        query = "SELECT username, average_score FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE average_score > 0 ORDER BY average_score DESC LIMIT 5;"
+        query = "SELECT username, average_score FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE average_score > 0 ORDER BY average_score DESC, average_score_updated_at ASC LIMIT 5;"
         return connectToMySQL('kawhoot_schema').query_db(query)
 
     @classmethod
     def get_quizcreators(cls):
-        query = "SELECT username, create_quiz_count FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE create_quiz_count > 0 ORDER BY create_quiz_count DESC LIMIT 5;"
+        query = "SELECT username, create_quiz_count FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE create_quiz_count > 0 ORDER BY create_quiz_count DESC, create_quiz_count_updated_at ASC LIMIT 5;"
         return connectToMySQL('kawhoot_schema').query_db(query)
     
     @classmethod
     def get_quiztakers(cls):
-        query = "SELECT attempt_count, username FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE attempt_count > 0 ORDER BY attempt_count DESC LIMIT 5;"
+        query = "SELECT attempt_count, username FROM usersummaries JOIN users ON usersummaries.user_id = users.id WHERE attempt_count > 0 ORDER BY attempt_count DESC, attempt_count_updated_at ASC LIMIT 5;"
         return connectToMySQL('kawhoot_schema').query_db(query)
